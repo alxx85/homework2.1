d4e30633d30697e6015dc7022181a477981c64ac
alxx85 <37430044+alxx85@users.noreply.github.com>
2018-09-21 00:46:41 +0700

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,30 +1,33 @@
-from pprint import pprint
+import module
 
-cook_book = {}
-with open('cookbook.txt') as f:
-    for line in f:
-        key = line.strip()
-#        print(key)
-        if key not in cook_book.keys():
-            cook_book[key] = list()
-        quantity = f.readline().strip()
-        i = 0
-        line_list = []
+def open_book():
+    cook_book = {}
+    list_book = ['ingridient_name', 'quantity', 'measure']
+    with open('cookbook.txt') as f:
+        for line in f:
+            key = line.strip()
+            if key not in cook_book:
+                cook_book[key] = list()
+            quantity = f.readline().strip()
+            i = 0
 
-        while i < int(quantity):
-            lines = f.readline().strip()
-            lines_list = lines.split(' | ')
-            value = dict.fromkeys(['ingridient_name', 'quantity', 'measure'])
-            value['ingridient_name'] = lines_list[0]
-            value['quantity'] = lines_list[1]
-            value['measure'] = lines_list[2]
-            cook_book[key].append(value)
-            i += 1
-        f.readline()
+            while i < int(quantity):
+                lines = f.readline().strip()
+                lines_list = lines.split(' | ')
+                value = dict()
+                for lst, lines_numb in enumerate(lines_list):
+                    value[list_book[lst]] = lines_numb
+                cook_book[key].append(value)
+                i += 1
+            f.readline()
+    return cook_book
+
+
+book = open_book()
 
 dishes_list = list()
 print('Список рецептов из книги:')
-for key in cook_book.keys():
+for key in book:
     print(key)
 
 dishes_promt = input('\nВведите названия рецептов через запятую: ')
@@ -33,25 +36,9 @@ if ', ' in dishes_promt:
 else:
     dishes_list.append(dishes_promt)
 
-person = input('Введите колличество персон: ')
+person = int(input('Введите колличество персон: '))
 
-def get_shop_list_by_dishes(book_cook, dishes, person_count):
-    book = {}
-    for dishe in dishes:
-        quantity = 0
-        for list_book in book_cook[dishe]:
-            key = book_cook[dishe][quantity]['ingridient_name']
-            if key not in book.keys():
-                book[key] = dict.fromkeys(['quantity', 'measure'])
-                book[key]['quantity'] = int(book_cook[dishe][quantity]['quantity']) * int(person_count)
-                book[key]['measure'] = book_cook[dishe][quantity]['measure']
-            else:
-                book[key]['quantity'] += int(book_cook[dishe][quantity]['quantity']) * int(person_count)
-
-            quantity += 1
-
-    pprint(book)
-if (person != '') and (type(person) == int ):
-    get_shop_list_by_dishes(cook_book, dishes_list, person)
+if person != '':
+    module.get_shop_list_by_dishes(book, dishes_list, person)
 else:
     print('Неверно указанно колличество персон!!!')
