20150e1d28a548f28242f57238142820118e5c8c
alxx85 <37430044+alxx85@users.noreply.github.com>
2018-09-26 21:02:28 +0700

    Update module.py

--- module.py
+++ module.py
@@ -3,13 +3,13 @@ from pprint import pprint
 def get_shop_list_by_dishes(book_cook, dishes, person_count):
     book_recipe = {}
     for dishe in dishes:
-        for lst_numb in range(len(book_cook[dishe])):
-            key = book_cook[dishe][lst_numb]['ingridient_name']
+        for ingredient in book_cook[dishe]:
+            key = ingredient['ingridient_name']
             if key not in book_recipe:
                 book_recipe[key] = dict()
-                book_recipe[key]['quantity'] = int(book_cook[dishe][lst_numb]['quantity']) * int(person_count)
-                book_recipe[key]['measure'] = book_cook[dishe][lst_numb]['measure']
+                book_recipe[key]['quantity'] = int(ingredient['quantity']) * int(person_count)
+                book_recipe[key]['measure'] = ingredient['measure']
             else:
-                book_recipe[key]['quantity'] += int(book_cook[dishe][lst_numb]['quantity']) * int(person_count)
+                book_recipe[key]['quantity'] += int(ingredient['quantity']) * int(person_count)
 
     pprint(book_recipe)
